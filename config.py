f7eb6dec01711af710dca1a7d03537d9d3787e07
Mariam Kamal <marioma.kamal77@gmail.com>
2023-08-26 05:27:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 
 API_KEY = "36ad5d5ab5bce3c74f4ab5583d4dac0e"
 API_TOKEN = "ATTA43ec17f5204d0093ea7804739339839cfb2aa05e4729ccd6386224de5415def483B192B9"
-BOARD_NAME = "Test final"
-list_names = ["TO do", "Doing", "Done"]
-card_titles = ["Requirement Analysis", "Scenario Identification"]
+BOARD_NAME = "Automation Task"
+list_names = ["To Do", "Doing", "Done"]
+card_titles = ["Requirement Analysis", "Scenario Identification", "Test Case Preparation", "Test Execution"]
 username = "Mariam.ghaly@cis.asu.edu.eg"
 password = "P@ssw0rd@345"
 TO_DO_List_ID= None
 LIST_NAME = "To Do"
-card_title="Scenario Identification"
-new_card_description="this is updated card description"
-destination_list="Done"
+card_title="Test Execution"
+new_card_description="Using the prepared test cases, start coding the automation scripts"
+destination_list="Doing"
