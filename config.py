4180fa43cd6aa8d4696ab7e21579574686bb597a
Mariam Kamal <marioma.kamal77@gmail.com>
2023-08-26 07:09:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,12 @@
 API_KEY = "36ad5d5ab5bce3c74f4ab5583d4dac0e"
 API_TOKEN = "ATTA43ec17f5204d0093ea7804739339839cfb2aa05e4729ccd6386224de5415def483B192B9"
 BOARD_NAME = "Automation Task"
-list_names = ["To Do", "Doing", "Done"]
+list_names = [ "Doing", "Done","To do"]
 card_titles = ["Requirement Analysis", "Scenario Identification", "Test Case Preparation", "Test Execution"]
 username = "Mariam.ghaly@cis.asu.edu.eg"
 password = "P@ssw0rd@345"
 TO_DO_List_ID= None
-LIST_NAME = "To Do"
+LIST_NAME = "To do"
 card_title="Test Execution"
 new_card_description="Using the prepared test cases, start coding the automation scripts"
 destination_list="Doing"
